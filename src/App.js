9e1728390890254c12c574daa3752100fb13a011
Salih Ahmet <salihahmet.nuhoglu@gmail.com>
2022-12-08 18:04:52 +0300

    app update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React , {useReducer}from 'react';
 
-function App() {
-  return (
-    <div >
-      
-    </div>
-  );
+
+const ACTIONS = {
+  INCREMENT: 'increment' ,
+  DECREMENT: 'decrement'
 }
 
+function reducer (state,action) {
+  switch (action.type) {
+    case  ACTIONS.INCREMENT : 
+      return {count :state.count+1}
+    case 
+    ACTIONS.DECREMENT : 
+      return {count : state.count-1}
+    default:
+     return state
+  }
+}
+
+function App() {
+  
+  const [state,dispatch] = useReducer (reducer, {count :0});
+
+  function increment () {
+    dispatch({type:ACTIONS.INCREMENT})
+  }
+
+  function decrement () {
+    dispatch({type:ACTIONS.DECREMENT})
+  }
+  
+  return(
+    
+    <>
+      <button onClick={decrement}>-</button>
+      <span>{state.count}</span>
+      <button onClick={increment}>+</button>
+    
+    </>
+  )
+}
 export default App;
